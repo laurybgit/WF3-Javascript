1ef33de87dc87f313b650578132727e2e9b02b85
laurybgit <laury.bourbon@gmail.com>
2020-10-13 11:34:58 +0200

    ajout fichier variables

---
+++ js/variables.js
@@ -0,0 +1,6 @@
+// Les variables 
+
+let monSlip; // j'ai créé mon "tiroir" (=variable) qui s'appelle monSlip qui ne contient rien. Création de variable. 
+
+// ranger un truc dans monSlip : faire une affectation de variable. 
+monSlip="CK"; 
